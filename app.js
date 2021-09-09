5111556796b1a193b03b38b64ee93dc44abe32c0
jkim51804 <89821607+jkim51804@users.noreply.github.com>
2021-09-09 16:19:49 +0900

    [UPDATE] create bubble
    - create bubble by the color of the pad
    - add bubble animation

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const pads = document.querySelectorAll(".pads div");
 const audio = document.querySelectorAll(".audio");
+const visual = document.querySelector(".visual");
+const colors = ["red", "coral", "yellow", "green", "blue", "navy"];
 
 pads.forEach((element, index) =>
   element.addEventListener("click", function () {
     audio[index].currentTime = 0;
     audio[index].play();
+    createBubble(index);
   })
 );
+
+const createBubble = function (index) {
+  let bubble = document.createElement("div");
+  visual.appendChild(bubble);
+  bubble.style.backgroundColor = colors[index];
+  bubble.style.animation = "move 1s ease";
+  bubble.addEventListener("animationend", function () {
+    visual.removeChild(this);
+  });
+};
